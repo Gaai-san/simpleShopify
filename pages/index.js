0c2777f6bd9292c990f56c3cc54c949b74c7b400
“Gaai-san” <iwateangler@gmail.com>
2023-02-20 23:04:43 +0900

    shopifyからデータをfetchしたい

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,33 @@
-export default function Home({ products }) {
+import { getProductsInCollection, shopifyFetch } from "../lib/shopify";
+
+export default function Home() {
   return <div className="text-3xl">Starter Template</div>;
 }
+
+// vercelのドキュメントコピペバージョンです。
+// https://vercel.com/guides/building-ecommerce-sites-with-next-js-and-shopify
+
+export async function getAllProducts() {
+  return shopifyFetch({
+    query: `{
+        products(sortKey: TITLE, first: 100) {
+          edges{
+            node {
+              id
+              title
+              description
+            }
+          }
+        }
+      }`,
+  });
+}
+
+// 以下は動画の講座通りにやった場合です。
+// export async function getStaticProps() {
+//   const products = await getProductsInCollection();
+
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   };
+// }
